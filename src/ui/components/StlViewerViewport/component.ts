7e3651bf9774f25c31d71206ab5053af4ef888c5
Gastón I. Silva <givanse@gmail.com>
2019-10-01 17:40:42 -0700

    extract tm3 models

--- src/ui/components/StlViewerViewport/component.ts
+++ src/ui/components/StlViewerViewport/component.ts
@@ -1,4 +1,5 @@
 import Component, { tracked } from '@glimmer/component';
+import getTM3Models from './tesla-m3.model';
 
 const modelIds = {
   car: [1101, 1302],
@@ -34,7 +35,7 @@ export default class StlViewerViewport extends Component {
     this.carColor = '#b18f15';
     this.caliperColor = '#8916d1';
     this.rimColor = '#181818';
-    this.headLightsColor = '#9ec8ef';
+    this.headLightsColor = '#f1f1f1';
     this.chromeTrimColor = '#363636';
     this.windowsColor = '#9ec8ef';
   }
@@ -46,29 +47,15 @@ export default class StlViewerViewport extends Component {
   }
 
   private initStlViewwer(hostElement) {
-    const wheelsy = -475;
-    const calipersy = -440;
-    const frontwheelsx = 810;
-    const backwheelsx = 860;
-    const backwheelsz = -1350;
-    const backcalipersz = -1485;
 
-    const frontwheelsz = 1612.5;
-    const frontcalipersz = 1465;
-    const leftwheelsrotationy = 15.75; // ???
-    const leftDiscRotationX = 15; // ???
-
-    const tireColor = '#242424';
-    const rearLightsColor = '#910000';
-    const discColor = '#363636';
-    const windshieldColor = '#9ec8ef';
-    const windshieldOpacity = 0.5;
-
-    const headLightsColor = this.headLightsColor;
-    const rimColor = this.rimColor;
-    const carColor = this.carColor;
-    const caliperColor = this.caliperColor;
-    const chromeTrimColor = this.chromeTrimColor;
+    const colors = {
+      headLightsColor: this.headLightsColor,
+      rimColor: this.rimColor,
+      carColor: this.carColor,
+      caliperColor: this.caliperColor,
+      chromeTrimColor: this.chromeTrimColor,
+      windowsColor: this.windowsColor,
+    };
 
     const initParams = {
       allow_drag_and_drop: false,
@@ -80,97 +67,7 @@ export default class StlViewerViewport extends Component {
       all_loaded_callback: function() {
         this.all3dFilesLoaded = true;
       }.bind(this),
-      models: [
-        // All coordinates are offsets relative to the car model.
-        // Looking from the front
-        {id: modelIds.car[0], filename: 'rest_of_car.stl', color: carColor},
-
-        // front right
-        {filename: 'tire.stl',
-         x: frontwheelsx, y: wheelsy, z: frontwheelsz, color: tireColor},
-        {filename: 'disc_and_axle.stl', color: discColor,
-         x: frontwheelsx - 75, y: wheelsy, z: frontwheelsz},
-        {id: modelIds.rims[0], filename: 'rim.stl',
-         x: frontwheelsx, y: wheelsy, z: frontwheelsz, color: rimColor},
-        {id: modelIds.calipers[0], filename: 'caliper.stl',
-         color: caliperColor, x: frontwheelsx, y: calipersy, z: frontcalipersz},
-        {filename: 'rim_hub.stl', color: rimColor,
-         x: frontwheelsx + 75, y: wheelsy, z: frontwheelsz},
-
-        // back right
-        {filename: 'tire.stl', x: backwheelsx, y: wheelsy, z: backwheelsz, color: tireColor},
-        {filename: 'disc_and_axle.stl', color: discColor,
-         x: backwheelsx - 75, y: wheelsy, z: backwheelsz},
-        {id: modelIds.rims[1], filename: 'rim.stl', x: backwheelsx, y: wheelsy, z: backwheelsz, color: rimColor},
-        {id: modelIds.calipers[1], filename: 'caliper.stl',
-         color: caliperColor, x: backwheelsx, y: calipersy, z: backcalipersz},
-        {filename: 'rim_hub.stl', color: rimColor, x: backwheelsx + 75, y: wheelsy, z: backwheelsz},
-
-        // back left
-        {filename: 'tire.stl',
-         x: -backwheelsx, y: wheelsy, z: backwheelsz, rotationy: leftwheelsrotationy, color: tireColor},
-        {filename: 'disc_and_axle.stl', color: discColor,
-         rotationx: leftDiscRotationX, rotationy: leftwheelsrotationy,
-         x: -backwheelsx + 75, y: wheelsy, z: backwheelsz},
-        {id: modelIds.rims[2],filename: 'rim.stl',
-         x: -backwheelsx, y: wheelsy, z: backwheelsz, rotationy: leftwheelsrotationy, color: rimColor},
-        {id: modelIds.calipers[2], filename: 'caliper.stl',
-         x: -backwheelsx, y: calipersy, z: backcalipersz, color: caliperColor},
-        {filename: 'rim_hub.stl', color: rimColor,
-         x: -(backwheelsx + 75), y: wheelsy, z: backwheelsz},
-
-        // front left
-        {filename: 'tire.stl',
-         x: -frontwheelsx, y: wheelsy, z: frontwheelsz, rotationy: leftwheelsrotationy, color: tireColor},
-        {filename: 'disc_and_axle.stl',  color: discColor,
-         rotationx: leftDiscRotationX, rotationy: leftwheelsrotationy,
-         x: -frontwheelsx + 75, y: wheelsy, z: frontwheelsz},
-        {id: modelIds.rims[3],filename: 'rim.stl',
-         x: -frontwheelsx, y: wheelsy, z: frontwheelsz, rotationy: leftwheelsrotationy, color: rimColor},
-        {id: modelIds.calipers[3], filename: 'caliper.stl',
-         x: -frontwheelsx, y: calipersy, z: frontcalipersz, color: caliperColor},
-        {filename: 'rim_hub.stl', color: rimColor, x: -(frontwheelsx + 75), y: wheelsy, z: frontwheelsz},
-
-        {filename: 'windshield.stl',
-         color: windshieldColor, opacity: windshieldOpacity,
-         y: 300, z: 800},
-        {filename: 'Rear_glass_roof_and_window.stl', y: 500, z: -1230, color: '#333333'},
-        {filename: 'Front_glass_roof.stl', y: 600, z: -50, color: '#333333'},
-
-        {filename: 'Front_flashlights.stl', color: rearLightsColor, y: -380, z: 2200},
-        {filename: 'Rear_flashlights.stl', color: rearLightsColor, y: -415, z: -2245},
-
-        {id: modelIds.headLights[0], filename: 'front_headlights.stl', color: headLightsColor, y: -87, z: 2033},
-        {filename: 'Rear_lights.stl', color: rearLightsColor, y: 92, z: -2060},
-
-        {id: modelIds.chromeTrim[0], filename: 'Left_window_trim.stl',
-         color: chromeTrimColor, x: 740, y: 340, z: -275}, // right
-        {id: modelIds.chromeTrim[1], filename: 'Rightwindow_trim.stl',
-         color: chromeTrimColor, x: -740, y: 340, z: -275}, // left
-        {id: modelIds.car[1], filename: 'Side_mirror_covers.stl', color: carColor, y: 187, z: 733},
-        {id: modelIds.chromeTrim[2], filename: 'Side_mirrors_and_chrome.stl', color: chromeTrimColor, y: 165, z: 833},
-
-        {id: modelIds.chromeTrim[3], filename: 'Door_handles_and_side_cams.stl',
-         color: chromeTrimColor, y: -10, z: 100},
-
-        {id: modelIds.windows[0], filename: 'Front_left_window.stl',
-         color: this.windowsColor, opacity: windshieldOpacity,
-         x: 745, y: 340, z: 277}, // right window
-        {id: modelIds.windows[1], filename: 'Front_right_window.stl',
-         color: this.windowsColor, opacity: windshieldOpacity,
-         x: -745, y: 340, z: 277}, // left window
-        {id: modelIds.windows[2], filename: 'Rear_left_window.stl',
-         color: this.windowsColor, opacity: windshieldOpacity,
-         x: 720, y: 380, z: -905},
-        {id: modelIds.windows[3], filename: 'Rear_right_window.stl',
-         color: this.windowsColor, opacity: windshieldOpacity,
-         x: -720, y: 380, z: -905},
-
-        {id: modelIds.chromeTrim[4], filename: 'Rear_T_logo.stl',
-         color: chromeTrimColor, y: 143, z: -2346},
-        {id: modelIds.chromeTrim[5], filename: 'Front_T_logo.stl',
-         color: chromeTrimColor, y: -112, z: 2270},
-      ],
+      models: getTM3Models(modelIds, colors),
     };
     const StlViewer = window.StlViewer;
     return new StlViewer(hostElement, initParams);
---
+++ src/ui/components/StlViewerViewport/tesla-m3.model.ts
@@ -0,0 +1,123 @@
+export default function(modelIds, colors) {
+  const {
+    carColor,
+    headLightsColor,
+    rimColor,
+    caliperColor,
+    chromeTrimColor,
+    windowsColor,
+  } = colors;
+
+  const wheelsy = -475;
+  const calipersy = -440;
+  const frontwheelsx = 810;
+  const backwheelsx = 860;
+  const backwheelsz = -1350;
+  const backcalipersz = -1485;
+
+  const frontwheelsz = 1612.5;
+  const frontcalipersz = 1465;
+  const leftwheelsrotationy = 15.75; // ???
+  const leftDiscRotationX = 15; // ???
+
+  const tireColor = '#242424';
+  const rearLightsColor = '#910000';
+  const discColor = '#363636';
+  const windshieldColor = '#9ec8ef';
+  const windshieldOpacity = 0.5;
+
+  return [
+    // All coordinates are offsets relative to the car model.
+    // Looking from the front
+    {id: modelIds.car[0], filename: 'rest_of_car.stl', color: carColor},
+
+    // front right
+    {filename: 'tire.stl',
+      x: frontwheelsx, y: wheelsy, z: frontwheelsz, color: tireColor},
+    {filename: 'disc_and_axle.stl', color: discColor,
+      x: frontwheelsx - 75, y: wheelsy, z: frontwheelsz},
+    {id: modelIds.rims[0], filename: 'rim.stl',
+      x: frontwheelsx, y: wheelsy, z: frontwheelsz, color: rimColor},
+    {id: modelIds.calipers[0], filename: 'caliper.stl',
+      color: caliperColor, x: frontwheelsx, y: calipersy, z: frontcalipersz},
+    {filename: 'rim_hub.stl', color: rimColor,
+      x: frontwheelsx + 75, y: wheelsy, z: frontwheelsz},
+
+    // back right
+    {filename: 'tire.stl', x: backwheelsx, y: wheelsy, z: backwheelsz, color: tireColor},
+    {filename: 'disc_and_axle.stl', color: discColor,
+      x: backwheelsx - 75, y: wheelsy, z: backwheelsz},
+    {id: modelIds.rims[1], filename: 'rim.stl', x: backwheelsx, y: wheelsy, z: backwheelsz, color: rimColor},
+    {id: modelIds.calipers[1], filename: 'caliper.stl',
+      color: caliperColor, x: backwheelsx, y: calipersy, z: backcalipersz},
+    {filename: 'rim_hub.stl', color: rimColor, x: backwheelsx + 75, y: wheelsy, z: backwheelsz},
+
+    // back left
+    {filename: 'tire.stl',
+      x: -backwheelsx, y: wheelsy, z: backwheelsz, rotationy: leftwheelsrotationy, color: tireColor},
+    {filename: 'disc_and_axle.stl', color: discColor,
+      rotationx: leftDiscRotationX, rotationy: leftwheelsrotationy,
+      x: -backwheelsx + 75, y: wheelsy, z: backwheelsz},
+    {id: modelIds.rims[2],filename: 'rim.stl',
+      x: -backwheelsx, y: wheelsy, z: backwheelsz, rotationy: leftwheelsrotationy, color: rimColor},
+    {id: modelIds.calipers[2], filename: 'caliper.stl',
+      x: -backwheelsx, y: calipersy, z: backcalipersz, color: caliperColor},
+    {filename: 'rim_hub.stl', color: rimColor,
+      x: -(backwheelsx + 75), y: wheelsy, z: backwheelsz},
+
+    // front left
+    {filename: 'tire.stl',
+      x: -frontwheelsx, y: wheelsy, z: frontwheelsz, rotationy: leftwheelsrotationy, color: tireColor},
+    {filename: 'disc_and_axle.stl',  color: discColor,
+      rotationx: leftDiscRotationX, rotationy: leftwheelsrotationy,
+      x: -frontwheelsx + 75, y: wheelsy, z: frontwheelsz},
+    {id: modelIds.rims[3],filename: 'rim.stl',
+      x: -frontwheelsx, y: wheelsy, z: frontwheelsz, rotationy: leftwheelsrotationy, color: rimColor},
+    {id: modelIds.calipers[3], filename: 'caliper.stl',
+      x: -frontwheelsx, y: calipersy, z: frontcalipersz, color: caliperColor},
+    {filename: 'rim_hub.stl', color: rimColor, x: -(frontwheelsx + 75), y: wheelsy, z: frontwheelsz},
+
+    {filename: 'windshield.stl',
+      color: windshieldColor, opacity: windshieldOpacity, y: 310, z: 810},
+    {filename: 'Rear_glass_roof_and_window.stl',
+      color: '#222222', opacity: .93, y: 500, z: -1230},
+    {filename: 'Front_glass_roof.stl',
+      color: '#222222', opacity: .93, y: 600, z: -50},
+
+    {filename: 'Front_flashlights.stl', color: rearLightsColor, opacity: .9, y: -380, z: 2200},
+    {filename: 'Rear_flashlights.stl', color: rearLightsColor, opcaity: .9, y: -415, z: -2245},
+
+    {id: modelIds.headLights[0], filename: 'front_headlights.stl',
+      color: headLightsColor, opacity: .87, y: -87, z: 2033},
+    {filename: 'Rear_lights.stl',
+      color: rearLightsColor, opacity: .9, y: 92, z: -2060},
+
+    {id: modelIds.chromeTrim[0], filename: 'Left_window_trim.stl',
+      color: chromeTrimColor, x: 740, y: 340, z: -275}, // right
+    {id: modelIds.chromeTrim[1], filename: 'Rightwindow_trim.stl',
+      color: chromeTrimColor, x: -740, y: 340, z: -275}, // left
+    {id: modelIds.car[1], filename: 'Side_mirror_covers.stl', color: carColor, y: 187, z: 733},
+    {id: modelIds.chromeTrim[2], filename: 'Side_mirrors_and_chrome.stl', color: chromeTrimColor, y: 165, z: 833},
+
+    {id: modelIds.chromeTrim[3], filename: 'Door_handles_and_side_cams.stl',
+      color: chromeTrimColor, y: -10, z: 100},
+
+    {id: modelIds.windows[0], filename: 'Front_left_window.stl',
+      color: windowsColor, opacity: windshieldOpacity,
+      x: 745, y: 340, z: 277}, // right window
+    {id: modelIds.windows[1], filename: 'Front_right_window.stl',
+      color: windowsColor, opacity: windshieldOpacity,
+      x: -745, y: 340, z: 277}, // left window
+    {id: modelIds.windows[2], filename: 'Rear_left_window.stl',
+      color: windowsColor, opacity: windshieldOpacity,
+      x: 720, y: 380, z: -905},
+    {id: modelIds.windows[3], filename: 'Rear_right_window.stl',
+      color: windowsColor, opacity: windshieldOpacity,
+      x: -720, y: 380, z: -905},
+
+    {id: modelIds.chromeTrim[4], filename: 'Rear_T_logo.stl',
+     color: chromeTrimColor, y: 143, z: -2346},
+    {id: modelIds.chromeTrim[5], filename: 'Front_T_logo.stl',
+     color: chromeTrimColor, y: -112, z: 2270},
+  ];
+}
